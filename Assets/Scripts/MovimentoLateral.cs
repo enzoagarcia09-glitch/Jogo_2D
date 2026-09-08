8228d16189edddc306ae20661d6dc55a1dfa6665
enzooag <enzoagarcia09@gmail.com>
2026-09-08 11:57:03 -0300

    Movimento do Player Criado
    Foi codado o movimento do player

---
+++ Assets/Scripts/MovimentoLateral.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MovimentoLateral : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+
+    public float horizontal;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+      horizontal = Input.GetAxisRaw("Horizontal");
+        transform.position += Vector3.right * horizontal * speed * Time.deltaTime;
+
+
+
+    }
+}
