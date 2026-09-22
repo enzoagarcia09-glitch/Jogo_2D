4d2d2b3a95439dd9ee7a217b44c4d9b5add50a45
enzooag <enzoagarcia09@gmail.com>
2026-09-22 09:19:25 -0300

    Código Salto
    Foi feito o código do salto para que seja reconhecido quando ele caia no chão e pule de maneira correta , sem ficar com pulos infinitos

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,6 +5,8 @@ public class player : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private bool isGrounded = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -16,10 +18,28 @@ public class player : MonoBehaviour
 
         rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y); //Vai aplicar a velocidade horizontal
         
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded )
         {
 
-            rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); //Vai adiconar o salto
+            rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); //Vai adicionar o salto
         }
     }
+
+    void OnCollisionEnter2D( Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; //Vai reconhecer quando o jogador cair no chão
+        }
+    }
+
+     void OnCollisionExit2D(Collision2D collision)
+    {
+
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded= false; //Vai reconhecer quando o jogador cair no chão
+        }   
+    }
+
 }
